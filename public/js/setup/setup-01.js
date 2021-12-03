505bb444a04431032588fd3c554da5879c2b6cad
Caio Willian <cwillian40@hotmail.com>
2021-12-03 00:23:15 -0300

    [04] 4. Renderização com WebGL

--- public/js/setup/setup-01.js
+++ public/js/setup/setup-01.js
@@ -1,1 +1,16 @@
-console.log(THREE);
+// Renderer
+const options = {
+  targetSelector: '#scene', // Onde o renderer deve desenhar > id #scene
+  with: 800,
+  height: 600
+}
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(
+  options.with, options.height
+);
+
+document.querySelector(
+  options.targetSelector
+).appendChild(renderer.domElement);
